a8b6ac589a7f0a070cc6b6b39a3af1a72f06e61f
kvagaja <122695440+kvagaja@users.noreply.github.com>
2023-02-20 17:15:10 +0000

    Brush animation on tracked hand

--- Assets/Scripts/StageSpawnBrush.cs
+++ Assets/Scripts/StageSpawnBrush.cs
@@ -7,12 +7,14 @@ using QuickVR;
 public class StageSpawnBrush : QuickStageBase
 {
     public GameObject prefab;
+    public Transform parent;
 
     protected override IEnumerator CoUpdate()
     {
         yield return new WaitForSeconds(3);
 
-        Instantiate(prefab);
+        Instantiate(prefab, parent);
+
     }
 
 
